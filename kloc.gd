7e48eb4df5c2d0682308a2a5dd86324da4ca68b5
Student FAIS UJ <Student FAIS UJ>
2018-03-17 17:50:41 +0100

    working movment

--- kloc.gd
+++ kloc.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var MOVE_SPEED = 1000;
+var velocity = Vector2()
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -12,15 +13,15 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
-func _process(delta):
+func _physics_process(delta):
 	var which = get_name()
 	# move up and down based on input
 	if Input.is_action_pressed(which+"_move_up") and position.y > 0:
-		position.y -= MOVE_SPEED * delta
+		apply_impulse( Vector2(0,0),  Vector2( 0, -MOVE_SPEED * delta)) 
 	if Input.is_action_pressed(which+"_move_down") and position.y < get_viewport_rect().size.y:
-		position.y += MOVE_SPEED * delta
+		apply_impulse( Vector2(0,0),  Vector2( 0, MOVE_SPEED * delta)) 
 	if Input.is_action_pressed(which+"_move_left") and position.x > 0:
-		position.x -= MOVE_SPEED * delta
+		apply_impulse( Vector2(0,0),  Vector2( -MOVE_SPEED * delta, 0)) 
 	if Input.is_action_pressed(which+"_move_right") and position.x < get_viewport_rect().size.x:
-		position.x += MOVE_SPEED * delta
+		apply_impulse( Vector2(0,0),  Vector2( MOVE_SPEED * delta, 0)) 
 	
