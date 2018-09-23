fd2a7cc422e62a00c288ac7c49f52fce7293701f
Kamil Mrzygłód <mrzyglod.kamil@gmail.com>
2018-09-23 17:03:15 +0200

    Music added

---
+++ v2/GameMusicPlayer.gd
@@ -0,0 +1,20 @@
+extends AudioStreamPlayer
+
+var start_music = load("res://music/start.ogg")
+var game_over_music = load("res://music/game_over.ogg")
+var win_music = load("res://music/start_music.ogg")
+
+func _ready():
+	pass
+
+func play_start_music():
+	self.stream = start_music
+	self.play()
+	
+func play_game_over_music():
+	self.stream = game_over_music
+	self.play()
+	
+func play_win_music():
+	self.stream = win_music
+	self.play()
--- v2/MainMenu.gd
+++ v2/MainMenu.gd
@@ -1,20 +1,26 @@
 extends MarginContainer
 
 var started = false;
+onready var game_music_player = get_node("../../GameMusicPlayer")
 
 func _ready():
+
 	if globals.screen_type == globals.SCREEN_STATE.START:
 		_swap_screen("start_screen.png");
+		game_music_player.play_start_music()
 	elif globals.screen_type == globals.SCREEN_STATE.WIN:
 		_swap_screen("wygrana.png");
+		game_music_player.play_win_music()
 	elif globals.screen_type == globals.SCREEN_STATE.LOSE:
 		_swap_screen("game_over.png");
+		game_music_player.play_game_over_music()
 	
 func _input(event):	
 	if event is InputEventKey and not started:
 		if event.pressed and event.get_scancode() == 16777221:
 			set_visible(false)
 			started = true
+			game_music_player.play_start_music()
 			
 func swap_screen_to_win():
 	globals.screen_type = globals.SCREEN_STATE.WIN
@@ -23,7 +29,7 @@ func swap_screen_to_win():
 func swap_screen_to_lose():
 	globals.screen_type = globals.SCREEN_STATE.LOSE
 	_swap_screen("game_over.png");
-	
+
 func swap_screen_to_start():
 	globals.screen_type = globals.SCREEN_STATE.START
 	_swap_screen("start_screen.png");
